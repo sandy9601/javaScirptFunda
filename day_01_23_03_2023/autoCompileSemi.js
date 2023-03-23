dac2dd645ec8063efb56c0284f404a04593e5882
sandy9601 <darshanamsandeep375@gmail.com>
2023-03-23 15:39:47 +0530

    comments functionality

---
+++ day_01_23_03_2023/autoCompileSemi.js
@@ -0,0 +1,18 @@
+// Here, JavaScript interprets the line break as an “implicit” semicolon. This is called an automatic semicolon insertion.
+
+// In most cases, a newline implies a semicolon. But “in most cases” does not mean “always”!
+
+// There are cases when a newline does not mean a semicolon. For example:
+// We recommend putting semicolons between statements even if they are separated by newlines. 
+// This rule is widely adopted by the community. Let’s note once again – it is possible to leave out semicolons most of the time.
+//  But it’s safer – especially for a beginner – to use them.
+
+// console.log(3 +
+// 1
+// + 2)
+
+// ! here js ensures that at end of line semicolon will require
+
+//console.log("hi")
+const alert =alert("hi")
+[1,2].forEach(alert)
---
+++ day_01_23_03_2023/comments.js
@@ -0,0 +1,9 @@
+// this part
+
+console.log("hi") // continue to the secnod part
+
+// As time goes on, programs become more and more complex. 
+// It becomes necessary to add comments which describe what the code does and why.
+
+// Comments can be put into any place of a script. 
+// They don’t affect its execution because the engine simply ignores them
