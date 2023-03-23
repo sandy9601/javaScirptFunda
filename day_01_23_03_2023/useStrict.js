2a47480f3493e472a5c4db84d7044be9acb26bf8
sandy9601 <darshanamsandeep375@gmail.com>
2023-03-23 15:54:23 +0530

    variables

---
+++ day_01_23_03_2023/useStrict.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let a
+a=10
+
+console.log(a)
---
+++ day_01_23_03_2023/variables.js
@@ -0,0 +1,24 @@
+// ! varaible is a named memory location
+
+// ! in javascript we can use let and const and var as key word
+// ? thses are key words to store data and there are different in each type
+
+let message
+message="hello" // this is one of right way to create a varaible in javascript
+
+console.log(message)
+
+// * there are limitations to name a varaible in javascript
+
+// *The name must contain only letters, digits, or the symbols $ and _.
+// *The first character must not be a digit.
+
+// ! When the name contains multiple words, camelCase is commonly used. ex thisIsTheBestCaseForThisName
+
+
+
+let san-sana="hi" // ! here hyphone not allowed in naming a variable
+
+let 1sana="sandy" // ! name cannot start with number
+
+// * naming a variable is case sensitive apple is different from APPLE
