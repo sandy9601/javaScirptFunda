714a459380f5ad84d8f585a189430ef88b62ce4a
sandy9601 <darshanamsandeep375@gmail.com>
2023-03-23 15:11:05 +0530

    intrduction to js

---
+++ day_01_23_03_2023/day_01_23_03-2023.js
@@ -0,0 +1,14 @@
+// ! introduction to javascript
+// ?  What makes JavaScript unique?
+// There are at least three great things about JavaScript:
+
+// Full integration with HTML/CSS.
+// Simple things are done simply.
+// Supported by all major browsers and enabled by default.
+//<!DOCTYPE HTML>
+
+// If we have a lot of JavaScript code, we can put it into a separate file.
+
+// Script files are attached to HTML with the src attribute:
+
+// <script src="/path/to/script.js"></script>
